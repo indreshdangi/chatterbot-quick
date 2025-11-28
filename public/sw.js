3b132cb8632614dc984bb9cb7f3e9344fc8b32ef
Indresh Dangi <indreshthakur541@gmail.com>
2025-11-28 21:21:38 +0530

    Added PWA support: manifest + service worker

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'indservices-cache-v1';
+
+const ASSETS = [
+  '/',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
